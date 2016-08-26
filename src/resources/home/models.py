58183876c5ec025efb4838930bd1a43d70c0528c
MalusGreen <kevin97illu@gmail.com>
2016-08-26 16:18:07 -0400

    Created Dynamic Dependancies
    Allows for minimal editing from the wagtail admin interface.

--- src/resources/home/models.py
+++ src/resources/home/models.py
@@ -3,7 +3,31 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
-
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
-    pass
+    logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+    ImageChooserPanel('logo')
+    ]
+
+class ProjectPage(Page):
+    projectImage = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+    ImageChooserPanel('projectImage')
+    ]
+
