68595ce309428b083bfde2ceb73091d8955d413a
MalusGreen <kevin97illu@gmail.com>
2016-09-06 22:21:55 -0400

    contact form

--- src/resources/home/models.py
+++ src/resources/home/models.py
@@ -5,7 +5,10 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
+
+from modelcluster.fields import ParentalKey
 
 class HomePage(Page):
     body = RichTextField(blank=True)
@@ -51,4 +54,23 @@ class InfoPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
         ImageChooserPanel('company_image')
+    ]
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', related_name='form_fields')
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = [
+        FieldPanel('title', classname="full title"),
+        FieldPanel('intro', classname="full"),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldPanel('to_address', classname="full"),
+            FieldPanel('from_address', classname="full"),
+            FieldPanel('subject', classname="full"),
+        ], "Email")
     ]
